07803529d2b00d9a0f7a254adcdd8dd888406cdf
Lasercut <tianxun.wang2004@gmail.com>
2018-04-10 16:48:09 +0100

    added some comments. added function to ensure user data file exists on startup

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 
 from flask import Flask, render_template, jsonify
-from flask_ask import Ask, statement, question, session, convert_errors
+from flask_ask import Ask, statement, question, session, convert_errors, context
 from afg import Supervisor
 import json
 import logging
+import os.path
 
 #logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
@@ -31,6 +32,23 @@ def close_user_session():
 def session_ended():
     close_user_session()
     return "", 200
+
+def ensure_user_data_file():
+    if not os.path.isfile(USER_DATA_PATH):
+        initial_data = [{
+                "name": "Bob",
+                "medications": {
+                    "planned": {},
+                    "actual": {}
+                }
+            }]
+
+        try:
+            with open(USER_DATA_PATH, "w+") as user_data_file:
+                json.dump(initial_data, user_data_file)
+        except:
+            close_user_session()
+            print "error ensure user data"
 
 def get_user_data(file_path):
     try:
@@ -62,6 +80,10 @@ def launched():
 
 @ask.intent("medTakenInfo")
 def medication_taken_info():
+    """
+    Returns the amount of medication taken
+    """
+    print "Device ID: {0}".format(context.System.device.deviceId)
     med_list = []
     med_data = get_user_data(USER_DATA_PATH)
     for medicine_taken, amount_taken in med_data[0]["medications"]["actual"].iteritems():
@@ -77,6 +99,11 @@ def medication_taken_info():
 
 @ask.intent("addMedToPlan", convert={"MedicationAmount":int, "MedicationName":"MedicationNameSlot"})
 def add_medication_to_plan(MedicationAmount, MedicationName):
+    """
+    Add medication to 'planned' list
+    - if medication already exist on 'planned' list, then increase the total amount
+    - if medication already exist on 'actual' list (already taken some meds), then do not reset count to 0
+    """
     if MedicationAmount is None or MedicationName is None:
         return question(render_template("ask_for_repeat"))
     else:
@@ -94,6 +121,9 @@ def add_medication_to_plan(MedicationAmount, MedicationName):
 
 @ask.intent("removeMedFromPlan", convert={"MedicationName":"MedicationNameSlot"})
 def remove_med_from_plan(MedicationName):
+    """
+    Deletes medication from both 'planned' and 'actual' lists
+    """
     if MedicationName is None:
         return question(render_template("ask_for_repeat"))
     else:
@@ -109,6 +139,9 @@ def remove_med_from_plan(MedicationName):
 
 @ask.intent("listMedFromPlan")
 def list_med_from_plan():
+    """
+    Returns the amount and type of medication from the 'planned' list
+    """
     med_data = get_user_data(USER_DATA_PATH)
     med_list = []
     for medicine, ammount in med_data[0]["medications"]["planned"].iteritems():
@@ -120,4 +153,5 @@ def list_med_from_plan():
 
 
 if __name__ == '__main__':
+    ensure_user_data_file()
     app.run(debug=True)
